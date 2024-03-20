7bf5f3f97401817bb0ff4712bdd607239eb6d2ff
SaiRam Ganna <sairam.g@beehyv.com>
2024-03-20 20:09:45 +0530

    Add workflow to upload code as zip file

--- app/app/api/api.py
+++ app/app/api/api.py
@@ -11,8 +11,8 @@ from app.api.endpoints import (
 api_router = APIRouter()
 api_router.include_router(normal_router, prefix="")
 api_router.include_router(batch_group_router, prefix="/batch_group", tags=["batch-group"])
-api_router.include_router(batch_job_router, prefix="/batch_job", tags=["batch-job"])
-api_router.include_router(batch_job_details_router, prefix="/batch_job_details", tags=["batch-job-details"])
+api_router.include_router(batch_job_router, prefix="/batch-job", tags=["batch-job"])
+api_router.include_router(batch_job_details_router, prefix="/batch-job-details", tags=["batch-job-details"])
 api_router.include_router(
-    batch_job_event_details_router, prefix="/batch_job_event_details", tags=["batch-job-event-details"]
+    batch_job_event_details_router, prefix="/batch-job-event-details", tags=["batch-job-event-details"]
 )
